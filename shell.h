2a56ed662d767c27a4030282bdf0454289add811
IA-LINK <jyc.kom@gmail.com>
2024-02-27 18:39:37 -0800

    Update main.c

--- shell.h
+++ shell.h
@@ -13,8 +13,14 @@
 #include <fcntl.h>
 #include <errno.h>
 
-#define MAX_INPUT_LEN 1024
-void execute_child_process(char *command, char *args[])
+#define MAX_INPUT_len 1024
+#define MAX_INPUT_LEN 256
+
+char exe_command(char *command_with_args);
+void execute_child_process(char *command, char *args[]);
+char *_getenv(const char *name);
+char *get_path(char *command);
+void display_prompt(void);
 
 
 #endif
