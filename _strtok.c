30c86b299e022db1cf02cc9d0bef26e5aa4e8b08
IA-LINK <jyc.kom@gmail.com>
2024-03-04 21:14:39 -0800

    Re-update

---
+++ _strtok.c
@@ -0,0 +1,43 @@
+#include "shell.h"
+
+/**
+ * _strtok - custom implementation of strtok function
+ * @str: the string to tokenize
+ * @delim: the delimiter characters
+ *
+ * Return: a pointer to the next token, or NULL if no more tokens are found
+ */
+char *_strtok(char *str, const char *delim)
+{
+	static char *last_pos;
+
+	char *start;
+
+	if (str != NULL)
+	{
+		last_pos = str;
+	}
+	else if (last_pos == NULL)
+	{
+		return (NULL);
+	}
+	while (*last_pos != '\0' && strchr(delim, *last_pos) != NULL)
+	{
+		last_pos++;
+	}
+	if (*last_pos == '\0')
+	{
+		return (NULL); /* If no more tokens left, return NULL */
+	}
+	start = last_pos;
+	while (*last_pos != '\0' && strchr(delim, *last_pos) == NULL)
+	{
+		last_pos++;
+	}
+	if (*last_pos != '\0')
+	{
+		*last_pos = '\0';
+		last_pos++;
+	}
+	return (start);
+}
